b71d86d5856b37f95e4416c8eb41f87d38b3e737
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-02-26 20:10:16 +0100

    fix the syntax error

--- server/models/studentSchema.js
+++ server/models/studentSchema.js
@@ -1,52 +1,53 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const studentSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true
+const studentSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    linkedinUrl: {
+      type: String,
+    },
+    languages: {
+      type: [String],
+      required: true,
+    },
+    program: {
+      type: String,
+      required: true,
+    },
+    background: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    cohort: {
+      type: Number,
+      required: true,
+    },
+    projects: {
+      type: [Schema.Types.Mixed],
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  phone: {
-    type: String,
-    required: true
-  },
-  linkedinUrl: {
-    type: String
-  },
-  languages: {
-    type: [String],
-    default: []  
-  },
-  program: {
-    type: String,
-    required: true
-  },
-  background: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String
-  },
-  cohort: {
-    type: Number,
-    required: true
-  },
-  projects: {
-    type: [Schema.Types.Mixed],
-    default: []  
-}, {
-  timestamps: true  
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = model('Student', studentSchema);
+module.exports = model("Student", studentSchema);
