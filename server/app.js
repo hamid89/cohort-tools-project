7144eb5f0a0319813feef804ae49e254fbb7d17b
Nobourg_Sebastien <nobourg.sebastien@gmail.com>
2025-02-26 17:39:36 +0100

    commit for pull

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const { MongoClient } = require(`mongodb`);
 const PORT = 5005;
 
+const mongoURI = "mongodb://localhost:27017";
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,20 +21,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
-app.get("/api/cohorts",(req,res,next) => {
-  res.sendFile(__dirname + '/cohorts.json')
-})
-app.get("/api/students",(req,res,next) => {
-  res.sendFile(__dirname + '/students.json')
-})
-
+app.get("/api/cohorts", (req, res, next) => {
+  res.sendFile(__dirname + "/cohorts.json");
+});
+app.get("/api/students", (req, res, next) => {
+  res.sendFile(__dirname + "/students.json");
+});
 
 // START SERVER
 app.listen(PORT, () => {
